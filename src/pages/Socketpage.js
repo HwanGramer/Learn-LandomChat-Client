3509b8dccb470bfcdc91536893ee5750d90b1431
mang <mang4371@gmail.com>
2022-11-14 20:20:03 +0900

    1114

--- src/pages/Socketpage.js
+++ src/pages/Socketpage.js
@@ -7,6 +7,7 @@ function Socketpage({socket}) {
     const [myId , setMyId] = useState('');
     const [chatMsg , setChatMsg] = useState('');
     const [chatList , setChatList] = useState([]);
+    const [callerId , setCallerId] = useState([]);
 
     useEffect(()=>{
         socket.emit('GETmySocketId' , function(socketId){ //? 나의 소켓아이디 가져옴
@@ -22,6 +23,10 @@ function Socketpage({socket}) {
         socket.on('GETuserList' , function(userList){ //? 유저목록을 실시간으로 보내줌.
             setUserList(userList);
         })
+
+        socket.on('chatCall' , function(callId){
+            setCallerId(callerId => [...callerId,callId]);
+        })
     },[])
 
     const chatSend = ()=>{
@@ -31,7 +36,19 @@ function Socketpage({socket}) {
     const individualChat = (id)=>{
         //? 개인챗으로 넘어가면 기본방에서 나가져야됨.
         socket.emit('POSTleave' , 'DefaultRoom' , ()=>{
-            navi('/individualChat/'+id);
+            navi('/individualChat/'+id); //? 나가졌다면 개인톡페이지로 넘어감.
+            socket.emit('POSTcall' , id)
+        })
+    }
+
+    const callMsg = (callId)=>{
+        socket.emit('POSTuserCheck' ,callId , (noFindId)=>{ //? 메세지를 받을때 그 유저가 접속중인가 확인하기.
+            if(!noFindId){
+                alert('상대가 접속을 해제하였습니다.');
+                setCallerId(callerId => callerId.filter((e)=> e!==callId)) //? 신청건 사람이 접속해재하면 아이콘 하나 사라짐.
+            }else{
+                console.log(callId);
+            }
         })
     }
 
@@ -39,7 +56,18 @@ function Socketpage({socket}) {
     <div>
         <h1 style={{'color': 'blue'}}>FAKE LIKE 랜덤채팅</h1>
         <h2>내 아이디 <br></br>{myId}</h2>
-
+        
+        <div style={{'width' : '100%' , 'border':'1px solid black' , 'display':'flex' ,'flexDirection':'row','flexWrap':'wrap'}}>
+        {
+            callerId.map((element , index)=>{
+                return(
+                    <div key={index} onClick={()=>{callMsg(element)}} style={{'textAlign':'center','width':'40px','height':'40px',
+                    'border':'1px solid pink','borderRadius':'50%','background':'red',color:'white'
+                    ,fontWeight:'800',display:'flex',alignItems:'center',justifyContent:'center'}}>NM</div>
+                )
+            })
+        }
+        </div>
         <div style={{'fontSize' : '13px'}}>광장대화</div>
         <div style={{'width':'100%' , 'border' : '1px solid black' , 'minHeight' : '200px'}}>
             {
